04447b27d7df48bcd5e3940fb7aba4ceabf50b48
courier-new <kellirockwell@mail.com>
2017-03-25 08:43:40 -0700

    added link to demo and removed console logs

--- fontr/fontr.js
+++ fontr/fontr.js
@@ -15,15 +15,12 @@ function getData() {
 	$.getJSON('fontsData.json', function(data) {
 	//$.getJSON('miniFontsData.json', function(data) { // for testing end game results only
 		fontsRemainingArr = fontsArr = data;
-		console.log(fontsArr);
  	});
 	
 	setTimeout(function(){ displayLegend(); }, 200);
 }
 
 function displayLegend() {
-	
-	console.log(fontsArr);
 
 	var output="<h2>Legend</h2>\n" +
 		"<p>(This will not be visible while you play!)</p>\n" +

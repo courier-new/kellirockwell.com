e373ba47442f9fd687bc490d6cc77f551427693a
courier-new <kellirockwell@mail.com>
2017-03-25 00:33:20 -0700

    updated fonts data array

--- fontr/fontr.js
+++ fontr/fontr.js
@@ -15,9 +15,9 @@ function getData() {
 
 	var data;
 	
-	$.getJSON('https://courier-new.github.io/fontr/fontsData.json', function(data) {
+	$.getJSON('fontsData.json', function(data) {
 	//$.getJSON('miniFontsData.json', function(data) { // for testing end game results only
-		fontsRemainingArr = fontsArr = data;
+		fontsRemainingArr = fontsArr = data.fonts;
  	});
 
 }

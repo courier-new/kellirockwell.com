626969f811bec157004c48d5f105cfa0c17c69d1
courier-new <kellirockwell@mail.com>
2017-04-04 20:19:16 -0700

    finished projects and resume sections

--- js/main-dist.js
+++ js/main-dist.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-	
+'use strict';
+
+$(document).ready(function () {
+
 	var sections = [];
 	addNavs();
 
@@ -17,58 +19,57 @@ $(document).ready(function() {
 		if ($windowWidth < 1025) {
 			$('#main-block').addClass('mob');
 			$('#main-block').removeClass('full');
-		} else {		
-			$('#main-block').removeClass('mob');			
+		} else {
+			$('#main-block').removeClass('mob');
 			$('#main-block').addClass('full');
 		}
-		
 	}
-	$(window).resize(function(){
+	$(window).resize(function () {
 		checkWidths();
 	});
 	checkWidths();
 
-	$('.github').hover(function() {
+	$('.github').hover(function () {
 		$('.github').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.github').removeClass('hover');
 	});
-	$('.email').hover(function() {
+	$('.email').hover(function () {
 		$('.email').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.email').removeClass('hover');
 	});
-	$('.linkedin').hover(function() {
+	$('.linkedin').hover(function () {
 		$('.linkedin').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.linkedin').removeClass('hover');
 	});
-	$('.facebook').hover(function() {
+	$('.facebook').hover(function () {
 		$('.facebook').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.facebook').removeClass('hover');
 	});
-	$('.codepen').hover(function() {
+	$('.codepen').hover(function () {
 		$('.codepen').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.codepen').removeClass('hover');
 	});
-	$('.resume').hover(function() {
+	$('.resume').hover(function () {
 		$('.resume').addClass('hover');
-	}, function() {
+	}, function () {
 		$('.resume').removeClass('hover');
 	});
 
-	$('#logo-holder img').hover(function() {
+	$('#logo-holder img').hover(function () {
 		var transform = "";
 		if (!$(this).closest('.right-side').is('.mob')) {
 			transform += "translate(-50%, -50%)";
 		}
 		if ($(this).is('.hovering')) {
-			transform += " rotateY(360deg)";				
+			transform += " rotateY(360deg)";
 			$(this).removeClass('hovering');
 		} else {
-			transform += " rotateY(180deg)";			
+			transform += " rotateY(180deg)";
 			$(this).addClass('hovering');
 		}
 		$(this).css('transform', transform);
@@ -77,74 +78,76 @@ $(document).ready(function() {
 	function showSection(sectionName, dir) {
 		// get window height
 		var offScreen = $(document).height();
-		$('.inner-col').not('.hidden').each(function() {
+		$('.inner-col').not('.hidden').each(function () {
 			// get column height
 			var colHeight = $(this).outerHeight();
 			// remember column should be hidden
 			$(this).addClass('hidden');
 			// move column to just off screen
-			var move = (dir === "up") ? {top: (offScreen + colHeight/1.5) + "px"} : {top: "-" + colHeight/1.5  + "px"};
+			var move = dir === "up" ? { top: offScreen + colHeight / 1.5 + "px" } : { top: "-" + colHeight / 1.5 + "px" };
 			$(this).animate(move, '3s');
 		});
 		$('.inner-col.' + sectionName).removeClass('hidden');
-		setTimeout(function() {
-			$('.inner-col.hidden').each(function() {
+		setTimeout(function () {
+			$('.inner-col.hidden').each(function () {
 				$(this).css('display', 'none');
-			 });
+			});
 		}, 500);
-		$('.inner-col').not('.hidden').each(function() {
+		$('.inner-col').not('.hidden').each(function () {
 			// get column height
 			var colHeight = $(this).outerHeight();
 			// move column to just off screen
-			var move = (dir === "up") ? "-" + colHeight/1.5 : offScreen + colHeight/1.5;
+			var move = dir === "up" ? "-" + colHeight / 1.5 : offScreen + colHeight / 1.5;
 			$(this).css('top', move + "px");
 			// make column visible
-		  	$(this).css('display', 'block');
-		  	$(this).animate({top: 50 + "%"}, '3s');
+			$(this).css('display', 'block');
+			$(this).animate({ top: 50 + "%" }, '3s');
 		});
 	}
 
-	$('#intro h3').click(function() {
+	$('#intro h3').click(function () {
 		showSection("about", "down");
 	});
 
-	$('.nav-item').not('.active').click(function() {
+	$('.nav-item').not('.active').click(function () {
 		var selSection = $(this).attr('class').split(" ")[1];
 		var currSection = "";
-		$(this).siblings().each(function() {
-			currSection += ($(this).is('.active')) ? $(this).attr('class').split(" ")[1] : "";
+		$(this).siblings().each(function () {
+			currSection += $(this).is('.active') ? $(this).attr('class').split(" ")[1] : "";
 		});
 		// check whether moving to section before (up) or after (down) current section
-		var dir = (sections.indexOf(currSection) > sections.indexOf(selSection)) ? "up" : "down";
+		var dir = sections.indexOf(currSection) > sections.indexOf(selSection) ? "up" : "down";
 		showSection(selSection, dir);
 	});
-
 
 	function addNavs() {
 		var nav = "";
 		var count = 1;
-		$('.inner-col').each(function() {
+		$('.inner-col').each(function () {
 			var className = $(this).attr('class').split(" ")[1];
 			// remember all the sections for directional navigation
 			sections.push(className);
 			nav += "<div class='nav-item " + className + "'>\n";
 			nav += className;
 			nav += "\n</div>\n";
-			if (count !== $('.inner-col').length) { // for all but the last nav item
+			if (count !== $('.inner-col').length) {
+				// for all but the last nav item
 				nav += "<span> ♦ </span>\n";
 			}
 			count++;
 		});
 
-		$('.nav').each(function() {
+		$('.nav').each(function () {
 			$(this).html(nav);
 			var className = $(this).closest('.inner-col').attr('class').split(" ")[1];
-			$(this).find('.nav-item.' + className).each(function() {$(this).addClass('active');});
+			$(this).find('.nav-item.' + className).each(function () {
+				$(this).addClass('active');
+			});
 		});
 	}
 
 	function switchMeters(boxToShow) {
-		$('.about .box').each(function() {
+		$('.about .box').each(function () {
 			if ($(this).is('.' + boxToShow)) {
 				$(this).removeClass('hidden').css('display', 'block');
 			} else {
@@ -153,10 +156,10 @@ $(document).ready(function() {
 		});
 	}
 
-	$('#meters span').click(function() {
+	$('#meters span').click(function () {
 		if ($(this).is('.inactive')) {
 			var choice = $(this).text().toLowerCase();
-			$('#meters span').each(function() {
+			$('#meters span').each(function () {
 				$(this).removeClass('active');
 				$(this).addClass('inactive');
 			});
@@ -164,5 +167,5 @@ $(document).ready(function() {
 			$(this).addClass('active');
 			switchMeters(choice);
 		}
-	});	
+	});
 });
---
+++ js/projects.js
@@ -0,0 +1,189 @@
+$(document).ready(function() {	
+
+	var projData;
+	getProjects();
+	setTimeout(function(){
+		addProjects();
+	}, 200);
+
+	var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio voluptatibus enim esse quis quaerat quam suscipit necessitatibus provident, nostrum perspiciatis voluptatem perferendis dolor, hic officia ipsam laboriosam possimus doloremque tenetur atque aspernatur? Molestias sit nulla vel, dolores ab incidunt officia veritatis. Iusto recusandae dolore unde repellat vel ipsa iste accusamus?";
+	var lorem2 = " Nesciunt ad ratione quis consequuntur doloribus animi in architecto itaque delectus esse consectetur iste nobis voluptatum, quibusdam alias eveniet enim rerum eos debitis odit.";
+	var lorem3 = "Saepe quisquam nobis, magni voluptate asperiores molestiae recusandae excepturi officia porro, nam maxime architecto corporis id nulla omnis, possimus adipisci eum animi alias cumque mollitia fuga dignissimos commodi odio. Tempore, dolores, animi! Quisquam, ullam. Consectetur quasi, exercitationem. Sint soluta quae pariatur? Similique omnis ipsum optio perferendis tempore fugit aliquam odio, adipisci et tempora perspiciatis labore, sit aperiam ducimus suscipit. Nesciunt, doloremque, magnam.";
+	
+
+	function getProjects() {
+		$.getJSON('./js/projects.json', function(data) {
+			projData = data;		
+	 	});
+	}
+
+	function addProjects() {
+		var currProjList = "";
+		var oldProjList  = "";
+		$(projData.projects).each(function() {
+			var curr   = $(this)[0];
+			var output = "<li class='project-card-box'>\n";
+			output += "<div class='project-card'>\n";
+			output += "<div>\n";
+			output += "<span class='project-img' style='background-image:url(\"";
+			output += curr.logo;
+			output += "\");'></span>\n";
+			output += "<div>\n";
+			output += "<h1>" + curr.name + "</h1>\n";
+			output += "<span>" + curr.short + "</span>\n";
+			output += "</div>\n</div>\n</div>\n</li>\n";
+			if (curr.type === "current") {
+				currProjList += output;
+			} else {
+				oldProjList  += output;
+			}
+		});
+		$('.projects-list.current').html(currProjList);
+		$('.projects-list.old').html(oldProjList);
+	}
+
+	function toggleInfo(dir, proj) {
+		var card;
+		// Find matching project card
+		$('.project-card h1').each(function() {
+			if ($(this).text() == proj) {
+				card = $(this).closest('.project-card');
+			}
+		});
+		// Find data in projData from json
+		var projObj;
+		for (var i = 0; i < projData.projects.length; i++) {
+			if (projData.projects[i].name === proj) {
+				projObj = projData.projects[i];
+			}
+		}
+		var cardPos = card.position();
+		var middle = [];
+		middle.top = cardPos.top + card.height()/2;
+		middle.left = cardPos.left + card.width()/2;
+
+		if (dir == "out") {
+			// Form content of infoview
+			var content = "<span class='exit'>✖</span>\n";
+			content += "<div>\n";
+			content += "<h1><strong>" + proj;
+			if (projObj.link !== "") {
+				content += "<a href='" + projObj.link + "' target='_blank'>";
+				content += "<i class='fa fa-external-link' aria-hidden='true'></i>";
+				content += "</a>";
+			}
+			content += "</strong>" + projObj.subtitle + "</h1>\n";
+			content += "<div class='stats'>\n";			
+			content += "<div class='timescale'><div class= 'mini-head'><h2>Timescale</h2></div></div>";
+			content += "<div class='team'><div class= 'mini-head'><h2>Team</h2></div></div>";
+			content += "<div class='tags'><div class= 'mini-head'><h2>Tags</h2></div></div>";
+			content += "<div class='timescale'><span>";
+			var tsFormatted = projObj.timescale.split(" - ");
+			content += tsFormatted[0] + " -<br>";
+			content += tsFormatted[1] + "</span></div>\n";
+			content += "<div class='team'><span>";
+			content += projObj.team + "</span></div>\n";
+			content += "<div class='tags'><span>";
+			content += projObj.tags + "</span></div>\n";
+			content += "</div>\n";
+			content += "<p>" + projObj.short + "</p>\n";
+			var descArr = projObj.desc.split("\n");
+			for (var j = 0; j < descArr.length; j++) {
+				content += "<p>" + descArr[j] + "</p>\n";
+				if (descArr.length === 1) {
+					content += "<p>" + lorem + "</p>\n";
+					content += "<p>" + lorem2 + "</p>\n";
+					content += "<p>" + lorem3 + "</p>\n";
+				}
+			}			
+			content += "</div>";
+
+			$('.extra-info').html(content);
+			$('.extra-info').css({
+				'top': middle.top,
+				'left': middle.left
+			}).animate({
+				'top': '-=' + middle.top,
+				'left': '-=' + middle.left,
+				'width': 'toggle',
+				'height': 'toggle'
+			}).toggleClass('hidden');
+			$('.extra-info *').animate({
+				'opacity': 1
+			});
+		} else {
+			$('.extra-info *, .exit').animate({
+				'opacity': 0
+			});
+			$('.extra-info').animate({
+				'top': '+=' + middle.top,
+				'left': '+=' + middle.left,
+				'width': 'toggle',
+				'height': 'toggle'
+			}).toggleClass('hidden');
+			$('.left-side .overlay-container').animate({
+				'opacity': '0'
+			});
+		}
+	}
+
+	$('.nav-item').not('.active').click(function() {
+		// Make sure project infoview is not open when trying to switch section
+		if (!$('.extra-info').is('.hidden')) {
+			// If it is, toggle it
+			var proj = $('.extra-info').find('h1 strong').text();
+			toggleInfo("in", proj);
+		}
+	});
+
+	// Open infoview
+	$('body').on('click', '.project-card', function() {
+		var proj = $(this).find('h1').text();
+		toggleInfo("out", proj);
+	});
+
+	// Close infoview
+	$('body').on('click', '.extra-info .exit', function() {
+		var proj = $(this).parent().find('h1 strong').text();
+		toggleInfo("in", proj);
+	});
+
+	$('body').on('mouseenter', '.project-card', function() {
+		// Get overlay container
+		var dest = $('.left-side .overlay-container');
+		// Get name of hovered project
+		var head = $(this).find('h1').text();
+		// Only allow changing background if info view is hidden
+		if ($('.extra-info').not('.hidden').length === 0) {
+			// Cut any ongoing animations short
+			dest.stop(true);
+			// Search for hovered project's data
+			$(projData.projects).each(function() {
+				var curr = $(this)[0];
+				// When project is found
+				if (head === curr.name) {
+					// Set overlay's background image to url stored in data
+					dest.css('background-image', 'url("' + curr.screen + '")');
+					dest.find('.overlay-caption').html(curr.caption).animate({
+					});
+				}
+			});
+			// Bring overlay to full opaqueness
+			dest.animate({
+				'opacity': '1'
+			});
+		}
+	});
+
+	$('body').on('mouseleave', '.project-card', function() {
+		// Get overlay container
+		var dest = $('.left-side .overlay-container');
+		// Only allow changing background if info view is hidden
+		if ($('.extra-info').not('.hidden').length === 0) {
+			// Reduce overlay to full transparency
+			dest.animate({
+				'opacity': '0'
+			});
+		}
+	});
+});

1813486fb35d767231a863062dba3655e2f5ebf8
courier-new <kellirockwell@mail.com>
2017-05-24 03:55:35 -0700

    tweaking spinner transition

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ let everythingLoaded = setInterval(function() {
 	if (/loaded|complete/.test(document.readyState)) {
 		clearInterval(everythingLoaded);
 		setTimeout(function() {
-			$('#loading-overlay').addClass('loaded');
+			$('body').addClass('loaded');
 		}, 500);
 	}
 }, 10);

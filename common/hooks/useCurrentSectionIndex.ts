862c04d0708660aec42f3d2b979d8dcd431e8ef5
Kelli Rockwell <kellirockwell@gmail.com>
2020-04-26 16:07:03 -0700

    fix bug with unnecessary rerunning of effect due to refs rerendering, add timer to recalculate section index after scroll stops

--- common/hooks/useCurrentSectionIndex.ts
+++ common/hooks/useCurrentSectionIndex.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import map from 'lodash/map';
 import reduce from 'lodash/reduce';
+import useDeepCompareEffect from './useDeepCompareEffect';
 
 type ScrollPosition = {
   x: number;
@@ -119,7 +120,13 @@ const useCurrentSectionIndex = (
     () => void | undefined
   >();
 
-  useEffect(() => {
+  // We use a deep compare effect here because otherwise, the effect will run
+  // any time any of the refs re-renders, even though the refs are not changing
+  // in these cases. Since the refs re-render every time the sectionIndex
+  // changes (and even worse, every time the scroll percentage changes, since
+  // the two hooks are often used from the same component), this was causing
+  // massive amounts of re-running
+  useDeepCompareEffect(() => {
     // If any of the refs is null, break
     if (hasNullRefs(sectionRefs, parentRef)) return undefined;
 
@@ -130,83 +137,109 @@ const useCurrentSectionIndex = (
 
     // Throttle request to not overload with repaints
     let requestRunning: number | null = null;
+    // We start a timer when handling a scroll to check the scroll position
+    // again once the user finishes scrolling. This way, even with the
+    // throttling, we can be sure our section index is accurate at the end of
+    // the scroll. We assign the timer to a variable so that we can clear it
+    // every time the handler is re-run. That way, only the last timer (when the
+    // user actually stops scrolling) will fire
+    let lastOnScrollTimeout: NodeJS.Timeout | undefined;
 
     /** Handler to calculate the section starting positions and find the current
-     * section to attach to parent element scroll and resize listeners */
+     * section, to attach to parent element scroll and resize listeners */
     const calculateSectionIndex = (): void => {
-      // Get the current scroll position of the outer container
-      const { y: yScrollPos } = getElementScrollPosition(parentRef.current);
-
-      // Total scrollable height of the outer container
-      const scrollHeight = parentRefCurrentValue.scrollHeight || 0;
-      // Bounding height of the outer container
-      const height = parentRefCurrentValue.getBoundingClientRect()?.height || 0;
-
-      // Before we go into checking the y-positions of all the individual
-      // sections, we can first check for some easy wins if the current scroll
-      // position is all the way at the top or all the way at the bottom
-
-      // Check if the current scroll position + the bounded height of the outer
-      // container is equal to the total scrollable height of the outer
-      // container. If it is, we've hit the bottom of the page and can just
-      // return the last section index
-      if (yScrollPos + height === scrollHeight) {
-        setSectionIndex(sectionRefCurrentValues.length - 1);
-      }
-
-      // Check if the current scroll position is 0 or negative. If it is, we're
-      // at the top of the page and can just return the first section index
-      if (yScrollPos <= 0) {
-        setSectionIndex(0);
-      }
-
-      // Lock more computation-heavy calculations in throttled block
+      // Lock computation-heavy calculations in throttled block
       if (requestRunning === null) {
-        requestRunning = 1;
-        requestRunning = window.requestAnimationFrame(() => {
-          // Get the starting positions for each section
-          const sectionStartingPositions = getSectionStartingPositions(
-            sectionRefCurrentValues,
-            yScrollPos,
-          );
+        // Clear the previous resize timeout -- this way only the very last
+        // timer will fire
+        if (lastOnScrollTimeout) {
+          clearTimeout(lastOnScrollTimeout);
+        }
 
-          let i = 0;
-          let didSetIndex = false;
-          // Check section starting positions against current scroll position to
-          // determine the current section; the current section is the last
-          // section whose y position our scroll position is greater than
-          while (sectionStartingPositions.length > i) {
-            const currentSectionStartingPos = sectionStartingPositions[i];
-            // If the current y scroll position is still equal to or greater
-            // than the current section's starting position
-            if (yScrollPos >= currentSectionStartingPos) {
-              // If it's the last section, set that section as the index
-              if (i === sectionStartingPositions.length - 1) {
-                setSectionIndex(i);
-                didSetIndex = true;
-                break;
+        /** Callback to attach to animation frame request that contains the
+         * actual logic for calculating the section index */
+        const requestAnimationFrameCallback = (): void => {
+          // Get the current scroll position of the outer container
+          const { y: yScrollPos } = getElementScrollPosition(parentRef.current);
+
+          // Total scrollable height of the outer container
+          const scrollHeight = parentRefCurrentValue.scrollHeight || 0;
+          // Bounding height of the outer container
+          const height = parentRefCurrentValue.getBoundingClientRect()?.height || 0;
+
+          // For tracking the section index to set at the end
+          let newSectionIndex = -1;
+
+          // Before we go into checking the y-positions of all the individual
+          // sections, we can first check for some easy wins if the current scroll
+          // position is all the way at the top or all the way at the bottom
+
+          // Check if the current scroll position + the bounded height of the
+          // outer container is greater than or equal to the total scrollable
+          // height of the outer container (minus a buffer of 2). If it is,
+          // we've hit the bottom of the page and can just return the last
+          // section index
+          if (yScrollPos + height >= scrollHeight - 2) {
+            newSectionIndex = sectionRefCurrentValues.length - 1;
+          }
+
+          // Check if the current scroll position is 0 or negative. If it is, we're
+          // at the top of the page and can just return the first section index
+          if (yScrollPos <= 0) {
+            newSectionIndex = 0;
+          }
+
+          // Only proceed with heavier calculations if we couldn't set the index
+          // with one of these ways
+          if (newSectionIndex === -1) {
+            newSectionIndex = 0; // Default
+
+            // Get the starting positions for each section
+            const sectionStartingPositions = getSectionStartingPositions(
+              sectionRefCurrentValues,
+              yScrollPos,
+            );
+
+            let i = 0;
+            // Check section starting positions against current scroll position to
+            // determine the current section; the current section is the last
+            // section whose y position our scroll position is greater than
+            while (sectionStartingPositions.length > i) {
+              const currentSectionStartingPos = sectionStartingPositions[i];
+              // If the current y scroll position is still equal to or greater
+              // than the current section's starting position
+              if (yScrollPos >= currentSectionStartingPos) {
+                // If it's the last section, set that section as the index
+                if (i === sectionStartingPositions.length - 1) {
+                  newSectionIndex = i;
+                  break;
+                } else {
+                  // Otherwise, check the next section
+                  i += 1;
+                }
               } else {
-                // Otherwise, check the next section
-                i += 1;
+                // Otherwise, we've gone too far; set the section as the previous index
+                newSectionIndex = i - 1;
+                break;
               }
-            } else {
-              // Otherwise, we've gone too far; set the section as the previous index
-              setSectionIndex(i - 1);
-              didSetIndex = true;
-              break;
             }
           }
 
-          // If for some reason we couldn't set the section still, default to
-          // the first section
-          if (!didSetIndex) {
-            setSectionIndex(0);
-          }
+          setSectionIndex(newSectionIndex);
+        };
 
-          setTimeout(() => {
-            requestRunning = null;
-          }, 100);
-        });
+        requestRunning = 1;
+        requestRunning = window.requestAnimationFrame(requestAnimationFrameCallback);
+
+        setTimeout(() => {
+          requestRunning = null;
+        }, 100);
+
+        // Set a timer to check the scroll position again later
+        lastOnScrollTimeout = setTimeout(
+          () => window.requestAnimationFrame(requestAnimationFrameCallback),
+          200,
+        );
       }
     };
 

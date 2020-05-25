952577b712a74587b95506b540e85ca2d54a56b2
Kelli Rockwell <kellirockwell@gmail.com>
2020-05-24 18:02:45 -0700

    preload core fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,32 @@ class CustomDocument extends Document {
   render(): JSX.Element {
     return (
       <html lang="en" style={{ background: '#061826' }}>
-        <Head />
+        <Head>
+          <link
+            as="font"
+            crossOrigin=""
+            href="/fonts/Karla/Karla-Regular.ttf"
+            rel="preload"
+          />
+          <link
+            as="font"
+            crossOrigin=""
+            href="/fonts/TitilliumWeb/TitilliumWeb-Light.ttf"
+            rel="preload"
+          />
+          <link
+            as="font"
+            crossOrigin=""
+            href="/fonts/TitilliumWeb/TitilliumWeb-Bold.ttf"
+            rel="preload"
+          />
+          <link
+            as="font"
+            crossOrigin=""
+            href="/fonts/YesevaOne/YesevaOne-Regular.ttf"
+            rel="preload"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />

d06f6165b9348faf70df5d7f2a17831a7873268e
Kelli Rockwell <kellirockwell@gmail.com>
2021-01-18 22:37:57 -0800

    wrap other screens in `Screen`

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import React, { FC, useRef } from 'react';
 
 import MobileProjectCarousel from '../common/components/MobileProjectCarousel';
 import ProjectCard from '../common/components/ProjectCard';
+import Screen from '../common/components/Screen';
 import { MAIN_CONTENT_MAX_WIDTH, SIDEBAR_WIDTH } from '../common/constants/content-sizes';
 import useDisplaySize from '../common/hooks/useDisplaySize';
 import useScrollPositionController from '../common/hooks/useScrollPositionController';
@@ -17,32 +18,35 @@ const ProjectsScreen: FC = () => {
   const outerRef = useRef<HTMLDivElement>(null);
   useScrollPositionController(outerRef);
 
-  return displaySize === 'MOBILE' ? (
-    <MobileProjectCarousel projects={PROJECTS} />
-  ) : (
-    <>
-      <div
-        className="flex-column flex-align-center full-width padding-med scrollable-y"
-        ref={outerRef}
-      >
-        <div style={{ maxWidth: MAIN_CONTENT_MAX_WIDTH + SIDEBAR_WIDTH }}>
-          {map(PROJECTS, (project, index) => (
-            <div
-              className={index === PROJECTS.length - 1 ? '' : 'margin-med-bottom'}
-              key={project.name}
-            >
-              <ProjectCard
-                orientation={displaySize === 'SMALL' ? 'vertical' : 'horizontal'}
-                {...project}
-                tagSize="medium"
-                tagSpacing="0.6em"
-              />
-            </div>
-          ))}
+  const content =
+    displaySize === 'MOBILE' ? (
+      <MobileProjectCarousel projects={PROJECTS} />
+    ) : (
+      <>
+        <div
+          className="flex-column flex-align-center full-width padding-med scrollable-y"
+          ref={outerRef}
+        >
+          <div style={{ maxWidth: MAIN_CONTENT_MAX_WIDTH + SIDEBAR_WIDTH }}>
+            {map(PROJECTS, (project, index) => (
+              <div
+                className={index === PROJECTS.length - 1 ? '' : 'margin-med-bottom'}
+                key={project.name}
+              >
+                <ProjectCard
+                  orientation={displaySize === 'SMALL' ? 'vertical' : 'horizontal'}
+                  {...project}
+                  tagSize="medium"
+                  tagSpacing="0.6em"
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+
+  return <Screen>{content}</Screen>;
 };
 
 export default ProjectsScreen;
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,32 +1,36 @@
 import React, { FC } from 'react';
 
+import Screen from '../common/components/Screen';
+
 /**
  * Screen component for primary screen "Resume"
  */
 const ResumeScreen: FC = () => (
-  <div className="padding-med">
-    <h1>Resume</h1>
-    <section>Stuff 1</section>
-    <section>Stuff 2</section>
-    <section>Stuff 3</section>
-    <section>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam molestiae quas
-      quisquam nisi sunt similique obcaecati quia quidem iusto deserunt, recusandae dolor
-      molestias, culpa neque in, laudantium assumenda temporibus sequi! Lorem ipsum dolor
-      sit amet consectetur adipisicing elit. Veniam tempore minus provident repellendus
-      accusamus itaque quam doloribus facere, accusantium, quas praesentium omnis
-      veritatis. Voluptatum quo ducimus commodi, incidunt quasi quae! Lorem ipsum, dolor
-      sit amet consectetur adipisicing elit. Cupiditate eum cumque necessitatibus nobis
-      veniam reprehenderit, sequi facilis, dolorem iste expedita, voluptate tenetur unde
-      tempore numquam dicta perspiciatis id recusandae quisquam! Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Rerum iusto eius sint. Accusamus vel assumenda,
-      corporis maxime molestias atque animi quas cum quis maiores amet voluptatibus magni
-      error ratione officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Laboriosam qui reprehenderit illum quaerat architecto commodi exercitationem velit.
-      Eius ipsam, quod asperiores perspiciatis, nihil omnis tenetur saepe deserunt sed,
-      cumque magnam?
-    </section>
-  </div>
+  <Screen>
+    <div className="padding-med">
+      <h1>Resume</h1>
+      <section>Stuff 1</section>
+      <section>Stuff 2</section>
+      <section>Stuff 3</section>
+      <section>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam molestiae
+        quas quisquam nisi sunt similique obcaecati quia quidem iusto deserunt, recusandae
+        dolor molestias, culpa neque in, laudantium assumenda temporibus sequi! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Veniam tempore minus provident
+        repellendus accusamus itaque quam doloribus facere, accusantium, quas praesentium
+        omnis veritatis. Voluptatum quo ducimus commodi, incidunt quasi quae! Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Cupiditate eum cumque necessitatibus
+        nobis veniam reprehenderit, sequi facilis, dolorem iste expedita, voluptate
+        tenetur unde tempore numquam dicta perspiciatis id recusandae quisquam! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Rerum iusto eius sint.
+        Accusamus vel assumenda, corporis maxime molestias atque animi quas cum quis
+        maiores amet voluptatibus magni error ratione officiis? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Laboriosam qui reprehenderit illum quaerat
+        architecto commodi exercitationem velit. Eius ipsam, quod asperiores perspiciatis,
+        nihil omnis tenetur saepe deserunt sed, cumque magnam?
+      </section>
+    </div>
+  </Screen>
 );
 
 export default ResumeScreen;

3dbd9f0678032c0b7a4e7560652e3897142e60c2
Kelli Rockwell <kellirockwell@gmail.com>
2021-01-18 22:25:41 -0800

    retrieve tools list from API

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,10 +1,15 @@
 import find from 'lodash/find';
 import flatMap from 'lodash/flatMap';
-import React, { FC, ReactNode } from 'react';
+import React, { FC, useCallback, useMemo, useRef } from 'react';
 
-import ScreenContent from '../common/components/ScreenContent';
-import ABOUT_ME_SECTIONS from '../content/about-me';
-import { ContentRenderer, ContentSection } from '../content/utilities/types';
+import Screen from '../common/components/Screen';
+import SideNavMenu from '../common/components/SideNavMenu';
+import useSectionIndexController from '../common/hooks/sections/useSectionIndexController';
+import useSectionRefs from '../common/hooks/sections/useSectionRefs';
+import useScrollPositionController from '../common/hooks/useScrollPositionController';
+import buildAboutMeSections from '../content/about-me';
+import { Tool } from '../content/about-me/inside-my-toolbox';
+import { ContentSection, SectionRefsMap } from '../content/utilities/types';
 
 /* eslint-disable-next-line jsdoc/require-jsdoc */
 const FAKE_CONTENT = (key: string): JSX.Element => (
@@ -55,9 +60,9 @@ const FAKE_CONTENT = (key: string): JSX.Element => (
  * @param headingLevel the level of heading to render for this section (e.g.
  * `headingLevel: 1` => `h1`, `headingLevel: 2` => `h2`)
  */
-export const renderAboutMeSections: ContentRenderer<typeof ABOUT_ME_SECTIONS> = (
-  sections,
-  sectionRefs,
+export const renderAboutMeSections = (
+  sections: ContentSection<string, JSX.Element>[],
+  sectionRefs: SectionRefsMap,
   headingLevel = 1,
 ): JSX.Element[] => {
   let headingTag = `h${headingLevel}`;
@@ -104,16 +109,81 @@ export const renderAboutMeSections: ContentRenderer<typeof ABOUT_ME_SECTIONS> = 
 
 /**
  * Screen component for primary screen "About Me"
+ *
+ * @param props the functional component props
+ * @param props.tools array of `Tool`s I use, fetched from the server
  */
-const AboutMeScreen: FC = () => (
-  <ScreenContent
-    activePage="about-me"
-    containerClassName="padding-med"
-    renderSections={
-      renderAboutMeSections as ContentRenderer<ContentSection<string, ReactNode>[]>
-    }
-    sections={ABOUT_ME_SECTIONS}
-  />
-);
+const AboutMeScreen: FC<{ tools: Tool[] }> = ({ tools }) => {
+  const sections = useMemo(() => buildAboutMeSections(tools), [tools]);
+
+  /** Attach a section index and scroll position controller to the ref of the
+   * outermost scrollable container of the main screen content */
+  const ref = useRef<HTMLDivElement>(null);
+
+  const sectionRefsMap = useSectionRefs(sections);
+
+  // Measure the section starting positions for each section on this screen
+  useSectionIndexController(ref, sectionRefsMap);
+  const { reset } = useScrollPositionController(ref);
+
+  const resetScroll = useCallback(() => {
+    reset();
+    ref.current?.scrollTo({ top: 0 });
+  }, [reset]);
+
+  return (
+    <Screen
+      ref={ref}
+      resetScroll={resetScroll}
+      sideNav={<SideNavMenu contentSections={sections} />}
+    >
+      <div className="padding-med">{renderAboutMeSections(sections, sectionRefsMap)}</div>
+    </Screen>
+  );
+};
 
 export default AboutMeScreen;
+
+type ToolsResponse = {
+  tools: {
+    data: Tool[];
+  };
+};
+
+/** Fetch tools from API */
+export const getServerSideProps = async (): Promise<{
+  props: { tools: Tool[] };
+}> => {
+  let tools: Tool[] = [];
+
+  const query = `query {
+    tools {
+      data {
+        _id
+        name
+        marks
+      }
+    }
+  }`;
+
+  if (process.env.FAUNADB_GRAPHQL_ENDPOINT) {
+    const res = await fetch(process.env.FAUNADB_GRAPHQL_ENDPOINT, {
+      body: JSON.stringify({
+        query,
+      }),
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.FAUNADB_SECRET}`,
+        'Content-type': 'application/json',
+      },
+      method: 'POST',
+    });
+    const { data: responseData } = (await res.json()) as { data: ToolsResponse };
+
+    tools = responseData.tools.data;
+  }
+
+  return {
+    props: { tools },
+  };
+};
